9d60b1c29bceff7181db31d332e356ffb5b6b867
Oséias da Silva Martins <50338986+ozmartins@users.noreply.github.com>
2022-11-12 16:23:36 -0300

    Fix problems

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 const { ApolloServer, gql } = require('apollo-server');
+const { buildSubgraphSchema } = require('@apollo/subgraph');
 const { readFileSync } = require('fs');
 const path = require("path");
 const typeDefs = gql(readFileSync(path.resolve(__dirname, "./schema.graphql"), { encoding: 'utf-8' }));
 const resolvers = require('./resolvers');
 const TrackAPI = require('./datasources/track-api');
-const { buildSubgraphSchema } = require("@apollo/subgraph");
 
-const server = new ApolloServer({
-  schema: buildSubgraphSchema({ typeDefs, resolvers }),
-  dataSources: () => {
-    return {
-      trackAPI: new TrackAPI(),
-    };
-  },
-});
-
-server.listen({ port: process.env.PORT || 4001 }).then(({ url, port }) => {
+async function startApolloServer(typeDefs, resolvers) {
+  const server = new ApolloServer({
+    schema: buildSubgraphSchema({ typeDefs, resolvers }),
+    dataSources: () => {
+      return {
+        trackAPI: new TrackAPI(),
+      };
+    },
+  });
+  const { url, port } = await server.listen();
   console.log(`
     🚀  Server is running
     🔉  Listening on port ${port}
     📭  Query at ${url}
-  `);
-});
-
+  `)
+}
+startApolloServer(typeDefs, resolvers);
